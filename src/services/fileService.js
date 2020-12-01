8dc5a183da38031a523c56edca4306693f294841
gaujimann <gudjong19@ru.is>
2020-12-01 15:06:14 +0000

    finished fileService

--- src/services/fileService.js
+++ src/services/fileService.js
@@ -7,7 +7,7 @@ export const copyFile = async (file, newLocation) => FileSystem.copyAsync({
   to: newLocation,
 });
 
-const loadImage = async (fileName) => FileSystem.readAsStringAsync(`${contactDirectory}/${fileName}`, {
+const loadContact = async (fileName) => FileSystem.readAsStringAsync(`${contactDirectory}/${fileName}`, {
   encoding: FileSystem.EncodingType.Base64,
 });
 
@@ -18,6 +18,24 @@ export const addContact = async (contactLocation) => {
 
   return {
     name: fileName,
-    file: await loadImage(fileName),
+    file: await loadContact(fileName),
   };
+};
+
+const setupDirectory = async () => {
+  const dir = await FileSystem.degInfoAsync(contactDirectory);
+  if (!dir.exists) {
+    await FileSystem.makeDirectoryAsync(contactDirectory);
+  }
+};
+
+export const getAllContacts = async () => {
+  await setupDirectory();
+  const result = await FileSystem.readDirectoryAsync(contactDirectory);
+  return Promise.all(
+    result.map(async (fileName) => ({
+      name: fileName,
+      file: await loadContact(fileName),
+    })),
+  );
 };
