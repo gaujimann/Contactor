cedd3bba5d5aea2ca4f2e25e26406a3091152108
hjalti19 <hjalti19@ru.is>
2020-12-01 22:24:36 +0000

    contact details view in progress

---
+++ src/views/ContactDetails/styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  contactPhoto: {
+    marginTop: 50,
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+  },
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderColor: 'red',
+  },
+  contactText: {
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderColor: 'red',
+    justifyContent: 'flex-start',
+    fontSize: 16,
+    fontWeight: 'bold',
+    padding: 16,
+    color: 'white',
+  },
+});
