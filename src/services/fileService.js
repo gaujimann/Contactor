4c8611df71404a94ccaefae7d1083e97596c8154
gaujimann <gudjong19@ru.is>
2020-12-03 11:09:43 +0000

    fixed saving contacts with non latin characters in name

--- src/services/fileService.js
+++ src/services/fileService.js
@@ -1,4 +1,5 @@
 import * as FileSystem from 'expo-file-system';
+import latinize from 'latinize';
 
 const contactDirectory = `${FileSystem.documentDirectory}contacts`;
 
@@ -8,15 +9,13 @@ const loadContact = async (fileName) => {
 };
 
 export const addContact = async (contact) => {
-  const fileName = `${contactDirectory}/${contact.name}-${contact.id}.json`;
+  const fileName = `${contactDirectory}/${latinize(contact.name)}-${contact.id}.json`;
   await FileSystem.writeAsStringAsync(fileName, JSON.stringify(contact));
 };
 
 export const editContact = async (oldContact, contact) => {
-  const oldFileName = `${contactDirectory}/${oldContact.name}-${oldContact.id}.json`;
-  const fileName = `${contactDirectory}/${contact.name}-${contact.id}.json`;
-  console.log('filename: ', fileName);
-  console.log('Oldfilename', oldFileName);
+  const oldFileName = `${contactDirectory}/${latinize(oldContact.name)}-${oldContact.id}.json`;
+  const fileName = `${contactDirectory}/${latinize(contact.name)}-${contact.id}.json`;
   await FileSystem.moveAsync({
     from: oldFileName,
     to: fileName,
@@ -25,7 +24,7 @@ export const editContact = async (oldContact, contact) => {
 };
 
 export const deleteContact = async (contact) => {
-  const fileName = `${contactDirectory}/${contact.name}-${contact.id}.json`;
+  const fileName = `${contactDirectory}/${latinize(contact.name)}-${contact.id}.json`;
   await FileSystem.deleteAsync(fileName);
 };
 
